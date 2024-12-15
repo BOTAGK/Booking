a437a2c4cfed0b0ecc9216d12859586fc73ea0b7
Jakub Bogacz <jakubbogacz05@gmail.com>
2024-12-15 14:35:34 +0100

    przywitanie

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello Leader sie melduje");
         System.out.println("Hello world!");
         System.out.println("Hello od Adama");
+        System.out.println("Hello od Bogacz");
     }
 }
