4b41dfb59c0b5262c1bbfd28b0d6d42721f06b4f
Jakub Bogacz <jakubbogacz05@gmail.com>
2024-12-17 11:34:41 +0100

    Dodano skladowe do ApartmentBooking i podstawowe metody

--- src/Booking/ApartmentBooking.java
+++ src/Booking/ApartmentBooking.java
@@ -1,9 +1,25 @@
 package Booking;
 
 public class ApartmentBooking extends Booking {
+    private int roomCount;
+    private int rating;
 
-
-    public ApartmentBooking(String id, String name, String location, double price, Boolean status) {
+    public ApartmentBooking(String id, String name, String location, double price, Boolean status, int roomCount, int rating) {
         super(id, name, location, price, status);
+        this.roomCount = roomCount;
+        this.rating = rating;
+    }
+    public int getRoomCount() {
+        return roomCount;
+    }
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRoomCount(int roomCount) {
+        this.roomCount = roomCount;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
