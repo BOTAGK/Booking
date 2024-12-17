53babc97f31f3f2a1a4173dac35ea51e38c9115d
Imxantek <antekaminski@gmail.com>
2024-12-17 16:42:44 +0100

    Update EventTicketBooking.java
    dodalem getery, setery i pola

--- src/Booking/EventTicketBooking.java
+++ src/Booking/EventTicketBooking.java
@@ -1,9 +1,47 @@
 package Booking;
 
+import java.time.LocalDate;
+
 public class EventTicketBooking extends Booking {
-
-
-    public EventTicketBooking(String id, String name, String location, double price, String startDate, String endDate) {
+    String eventType;
+    String artistOrTeam;
+    int availableTickets;
+    LocalDate date;
+    public EventTicketBooking(String id, String name, String location, double price, String startDate, String endDate, String eventType, String artistOrTeam, int availableTickets, LocalDate date) {
         super(id, name, location, price, startDate, endDate);
+        this.eventType = eventType;
+        this.artistOrTeam = artistOrTeam;
+        this.availableTickets = availableTickets;
+        this.date = date;
+    }
+    public String getEventType() {
+        return eventType;
+    }
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+    public String getArtistOrTeam() {
+        return artistOrTeam;
+    }
+    public void setArtistOrTeam(String artistOrTeam) {
+        this.artistOrTeam = artistOrTeam;
+    }
+    public int getAvailableTickets() {
+        return availableTickets;
+    }
+    public void setAvailableTickets(int availableTickets) {
+        this.availableTickets = availableTickets;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public boolean reserveTickets(){
+        //tu bedzie rezerwowanie (nw czy ma byc boolean)
+    }
+    public boolean cancelBooking(){
+        //tutaj cancelowanie
     }
 }
