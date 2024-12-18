7485b4b709b6f9ea4490fb8f66e8d6ebab31309a
adamplatos <adam.platos.17@gmail.com>
2024-12-18 16:11:55 +0100

    Update BookingService.java

--- src/Main/BookingService.java
+++ src/Main/BookingService.java
@@ -12,24 +12,48 @@ public class BookingService {
 
     }
 
-    public Booking createBooking(User user, Booking booking, String startDate, String endDate){
+    public Booking createBooking(User user, Booking booking, String startDate, String endDate) {
         booking.setUser(user);
         booking.setStartDate(startDate);
         booking.setEndDate(endDate);
+        bookings.add(booking);
         return booking;
     }
 
-    public void cancelBooking(String bookingId){
-
+    public void cancelBooking(String bookingId) {
+        boolean found = false;
+        for(int i = 0; !found && i < bookings.size(); i++){
+            if(bookings.get(i).getId().equals(bookingId)){
+                bookings.remove(i);
+                found = true;
+            }
+        }
     }
 
-    public ArrayList<Booking>  getBookingsForUser(User user){
+    public ArrayList<Booking>  getBookingsForUser(User user) {
         return user.getBookings();
     }
 
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public ArrayList<User> getObservers() {
+        return observers;
+    }
+
     /*
-    public ArrayList<Booking> showBookings(SearchStrategy strategy){
-        return strategy.search();
+    public void setStrategy(SearchStrategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void showBookings(SearchStrategy strategy) {
+        bookings = strategy.search(bookings);
+        for(int i = 0; i < bookings.size(); i++){
+            System.out.println(bookings.get(i));
+        }
     }
      */
+
+
 }
