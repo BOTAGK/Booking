a176bb257cd82f35c1241fa43cd498aee97ca6c7
Kacper <kgazda27@gmail.com>
2024-12-19 15:35:51 +0100

    Version 3 - fixed user and BookingService added CarRental

--- src/Booking/CarRentalBooking.java
+++ src/Booking/CarRentalBooking.java
@@ -2,8 +2,38 @@ package Booking;
 
 public class CarRentalBooking extends Booking {
 
+    private String carType;
+    private String carModel;
+    private int rentLength;
 
-    public CarRentalBooking(String id, String name, String location, double price, String startDate, String endDate) {
+    public CarRentalBooking(String id, String name, String location, double price, String startDate, String endDate,
+                            String carType, String carModel, int rentLength) {
         super(id, name, location, price, startDate, endDate);
+        this.carType = carType;
+        this.carModel = carModel;
+        this.rentLength = rentLength;
     }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public int getRentLength() {
+        return rentLength;
+    }
+
+    /*
+    public boolean reserveService(){
+      tu będzie rezerwowanie samochodów
+    }
+
+
+    public boolean cancelBooking(){
+        tu anulowanie rezerwacji
+    }
+    */
 }
