288865df69ab2917b913808c47f2e42b75f05824
Veird <111653220+Veirdd@users.noreply.github.com>
2025-01-08 12:31:31 +0100

    hello

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("Hello od Bogacz");
         System.out.println("Hello imxantek");
         System.out.println("Hello kgazda");
+        System.out.println("Hello od rzezia");
         */
 
         /*
