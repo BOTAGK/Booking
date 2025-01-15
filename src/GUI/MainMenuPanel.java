a8e412072cfb3bf641bbe104110d1e9f69939fbb
Veird <111653220+Veirdd@users.noreply.github.com>
2025-01-16 00:04:36 +0100

    list

--- src/GUI/MainMenuPanel.java
+++ src/GUI/MainMenuPanel.java
@@ -3,15 +3,29 @@ package GUI;
 import Booking.*;
 import FilterStrategy.FilterManager;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
 import java.util.ArrayList;
 
 public class MainMenuPanel extends JPanel {
