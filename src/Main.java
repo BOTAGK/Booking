3098c92de2462e03e52e4da470edf5ade8e6094e
Jakub Bogacz <jakubbogacz05@gmail.com>
2025-01-16 22:05:56 +0100

    NAPRAWIONA deserializacja

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@ import GUI.LoginGui;
 import GUI.MainMenuGui;
 
 public class Main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         new Main().run();
     }
 
     public void run() {
-        // ResourceManager.getInstance().deseriaizeBookings();
-        // ResourceManager.getInstance().deseriaizeUsers();
-        ResourceManager.getInstance().readFileUser();
-        ResourceManager.getInstance().readFileApartmentBooking();
-        ResourceManager.getInstance().readFileCarRentalBooking();
-        ResourceManager.getInstance().readFileEventTicketBooking();
 
-        new MainMenuGui(new User(null, null, null, null, null));
+        ResourceManager.getInstance().deseriaizeBookings();
+        ResourceManager.getInstance().deseriaizeUsers();
+
+
+
+        new LoginGui();
+
+
     }
 }
